b011c02f1fb7b464e8c43045b6c81f01d77a476b
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-05 20:29:59 +0530

    [chriu] Checking Given Input Leap Or Not

---
+++ Basic_Core_Programmes_Day3/Basic_Core_Programmes_Day3/Leap_Year.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programmes_Day3
+{
+    public class Leap_Year
+    {
+        public void LaepYearCheck()
+        {
+            Console.WriteLine("Enter Year Number whether to Check Leap");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Year % 4 == 0)
+            {
+                Console.WriteLine(" {0} is a Leap Year:", Year);
+            }
+            else if (Year % 400 == 0)
+            {
+                Console.WriteLine(" {0} is a Leap Year:", Year);
+            }
+            else
+            {
+                Console.WriteLine(" {0} is not a Leap Year:", Year);
+            }
+
+        }
+    }
+}
---
+++ Basic_Core_Programmes_Day3/Basic_Core_Programmes_Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Basic_Core_Programmes_Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stat To Baisc Programmes");
+
+            Leap_Year obj = new Leap_Year();
+            obj.LaepYearCheck();
+        }
+    }
+}
